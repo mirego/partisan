dbab0bfd59bb0805228bfa9600bca515ca4879a5
Andrew Dedman <andrew.dedman@gmail.com>
2017-12-21 08:19:07 -0500

    Fix Column Name On Migration
    This didn't show itself as a problem until I added a self referential table, ie Users following other Users. If someone followed a user, and that user tried to follow them back, a unique constraint error on the db was triggered.

--- lib/generators/partisan/templates/migration.rb
+++ lib/generators/partisan/templates/migration.rb
@@ -9,7 +9,7 @@ class AddFollowsMigration < ActiveRecord::Migration
 
     add_index :follows, ['follower_id', 'follower_type'], name: 'index_partisan_followers'
     add_index :follows, ['followable_id', 'followable_type'], name: 'index_partisan_followables'
-    add_index :follows, ['follower_id', 'follower_type', 'followable_id', 'follower_type'], name: 'index_partisan_unique_follow', unique: true
+    add_index :follows, ['follower_id', 'follower_type', 'followable_id', 'followable_type'], name: 'index_partisan_unique_follow', unique: true
   end
 
   def down
